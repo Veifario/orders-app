a76e88aa8a68aed8cd42aa54c0d3edb78869f66a
Veifario <veifario.git@gmail.com>
2024-11-18 00:41:36 +0500

    feature: initial setup

---
+++ tailwind.config.js
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#F2BF1A",
+        secondary: "#F2BF1A14",
+        desert: "#D5C388",
+        background: "#F9F7F1",
+        gray: "#8E8E8E",
+        black: "#1F1F1F",
+        white: "#FFFFFF",
+        error: "#F11212",
+        success: "#18AE4A",
+      },
+    },
+  },
+  plugins: [],
+};
